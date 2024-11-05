ec8bb6d34705c700dd1fdf69ec1b5bf19ad0c6e8
Andrés <4145808@alu365.murciaeduca.es>
2024-11-05 14:20:20 +0100

    Agregar archivos de proyecto.

---
+++ Prueba/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Viajeros;
+using static Viajeros.Autenticacion;
+
+namespace Prueba
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var autenticacion = new Autenticacion();
+            var publicaciones = await autenticacion.ObtenerPublicaciones();
+
+            // Imprimir cada publicación en la consola
+            foreach (var publicacion in publicaciones)
+            {
+                Console.WriteLine($"ID: {publicacion.Id}, Título: {publicacion.Title}");
+            }
+
+            // Obtener comentarios
+            var comentarios = await autenticacion.ObtenerPublicaciones();
+            foreach (var comentario in comentarios)
+            {
+                Console.WriteLine($"Comentario ID: {comentario.Id}, Comentario: {comentario.Body}");
+            }
+
+            // Crear una nueva publicación
+            var nuevaPublicacion = new Post { UserId = 1, Title = "Nueva Publicación", Body = "Contenido de la nueva publicación" };
+            bool resultadoCreacion = await autenticacion.CrearPublicacion(nuevaPublicacion);
+            Console.WriteLine($"¿Publicación creada? {resultadoCreacion}");
+
+           
+            // Actualizar una publicación
+            /*var publicacionActualizada = new Post { UserId = 1, Title = "Título Actualizado", Body = "Contenido actualizado" };            
+            bool resultadoActualizacion = await autenticacion.ActualizarPublicacion(1, publicacionActualizada);
+            Console.WriteLine($"¿Publicación actualizada? {resultadoActualizacion}");*/
+
+        }
+    }
+}
